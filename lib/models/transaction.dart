0f6cd0ee1396ac76105d18a7573dfcfa3c606aa2
gguillhherrmme123-maker <gguillhherrmme123@gmail.com>
2026-09-15 18:46:09 -0300

    Update transaction.dart

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -14,4 +14,24 @@ class FinancialTransaction {
     required this.description,
     required this.date,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'type': type.name,
+      'amount': amount,
+      'description': description,
+      'date': date.toIso8601String(),
+    };
+  }
+
+  factory FinancialTransaction.fromJson(Map<String, dynamic> json) {
+    return FinancialTransaction(
+      id: json['id'],
+      type: json['type'] == 'income' ? TransactionType.income : TransactionType.expense,
+      amount: (json['amount'] as num).toDouble(),
+      description: json['description'],
+      date: DateTime.parse(json['date']),
+    );
+  }
 }
