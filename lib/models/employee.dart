2b70edbf2fb9ce8f89d7cfbcb831f5f80aea20c4
gguillhherrmme123-maker <gguillhherrmme123@gmail.com>
2026-09-15 16:01:00 -0300

    Create employee.dart

---
+++ lib/models/employee.dart
@@ -0,0 +1,13 @@
+class Employee {
+  String id;
+  String name;
+  String role;
+  String phone;
+
+  Employee({
+    required this.id,
+    required this.name,
+    required this.role,
+    required this.phone,
+  });
+}
